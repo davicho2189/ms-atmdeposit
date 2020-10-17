f5b164a428e2585ac2a6e4ef24bff374252b26f8
Cesar Guerrero <ce_david14@outlook.com.pe>
2020-10-16 21:33:00 -0500

    first commit

---
+++ src/main/java/com/atmdeposit/atm/MsAtmdepositApplication.java
@@ -0,0 +1,13 @@
+package com.atmdeposit.atm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsAtmdepositApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsAtmdepositApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atmdeposit/atm/MsAtmdepositApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atmdeposit.atm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsAtmdepositApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
